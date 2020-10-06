1d5534bf83239ca6b69308fc2c4ab4fea017ef0e
ajedkrap <mr.thedja@gmail.com>
2020-10-07 00:34:19 +0700

    submit

---
+++ index.js
@@ -0,0 +1,13 @@
+function tugas16(){
+
+  var x = [45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10]
+  min = Math.min(...x);
+
+  var y = [45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5 ]
+  max = Math.max(...y);
+
+  console.log("Nilai Minimal : ", min);
+  console.log("Nilai Maximal : ", max);
+}
+
+tugas16();
